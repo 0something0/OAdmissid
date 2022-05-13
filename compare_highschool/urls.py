eba63f7f3c428b0c4b9656e77bb6b93b2df932eb
samnsamn <sungjunnoh0802@gmail.com>
2022-05-13 03:57:13 -0400

    Implemented default Django login views

--- compare_highschool/urls.py
+++ compare_highschool/urls.py
@@ -18,5 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('compare_hs.urls')),
+    path('', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ]
